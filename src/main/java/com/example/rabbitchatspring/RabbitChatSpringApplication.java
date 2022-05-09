bde9439d05d8f67279bcfdd143b7efc8f673f38b
alex90bar <aleksandr.baranov@skillbox.ru>
2022-05-09 18:54:10 +0300

    simple fucntionality to enter chat and send messages via RabbitMQ realized

---
+++ src/main/java/com/example/rabbitchatspring/RabbitChatSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitchatspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitChatSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RabbitChatSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/rabbitchatspring/RabbitChatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitchatspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitChatSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
